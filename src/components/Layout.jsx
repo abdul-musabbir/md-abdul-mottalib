f8a2b19b17736015bba943d8544f19d3671109dc
Musabbir <musabbir.net@gmail.com>
2024-07-09 17:47:29 +0600

    add loading page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import Header from "./Header";
 
 export default function Layout({ children }) {
   return (
-    <div>
+    <div className="main">
       <Header />
       <div className="text-white">{children}</div>
     </div>
